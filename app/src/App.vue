2c93ddce247ef4b9d8e25dacff5c5818e1edd712
TalonChen <talonc@nycstudents.net>
2026-03-13 13:59:01 -0400

    hi

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div></div>
 </template>
 
-<style scoped></style>
+<script setup>
+//https://data.cityofnewyork.us/resource/mq6n-s45c.json
+</script>
+
+<style lang="scss" scoped></style>
